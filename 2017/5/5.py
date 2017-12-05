5a7b2c6aa933bdfcadb949ca5fc52665af10af8c
Matt Boler <failtolaunch28@gmail.com>
2017-12-05 13:25:20 -0600

    finished 5a

---
+++ 2017/5/5.py
@@ -0,0 +1,18 @@
+import sys
+
+def main(argv):
+    location = 0
+    step = 0
+    with open('input.txt', 'r') as myfile:
+       instructions = [int(instruction) for instruction in myfile]
+
+    while location < len(instructions):
+        offset = instructions[location]
+        location = location + offset
+        instructions[location - offset] += 1
+        step += 1
+        print(step)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
