e8a9bfd2210c9354d13072fe6800a39605038e4b
Matt Boler <meb0054@auburn.edu>
2019-11-13 15:51:49 -0600

    Complete 1b

--- 2018/01/soln.py
+++ 2018/01/soln.py
@@ -2,24 +2,44 @@
 
 INPUT_FILENAME = 'input.txt'
 
+def adjust_freq(line, freq):
+    operator = line[0]
+    magnitude = int(line[1:])
+    if operator == '+':
+        return freq + magnitude
+    else:
+        return freq - magnitude
+
 def part_1(filename):
     freq = 0
-    with open(INPUT_FILENAME) as f:
+    with open(filename) as f:
         for line in f:
             """ The important thing here is the first character of the line
             will either be a '-' or a '+', and the following characters
             are the decrement/increment amount """
-            operator = line[0]
-            magnitude = int(line[1:])
-            if operator == '+':
-                freq += magnitude
-            else:
-                freq -= magnitude
+            freq = adjust_freq(line, freq)
     return freq
+
+def part_2(filename):
+    freq = 0
+    frequencies = {freq}
+    """ Testing: Run through for just first iteration of file"""
+    while True:
+        with open(filename) as f:
+            for line in f:
+                freq = adjust_freq(line, freq)
+                if freq in frequencies:
+                    return freq
+                else:
+                    frequencies.add(freq)
+    print(sorted(frequencies))
+
+    return 'lol'
 
 
 if __name__ == '__main__':
     freq = part_1(INPUT_FILENAME)
-    print("Frequency: ", freq)
+    print("Frequency: (Part 1)", freq)
 
-
+    freq = part_2(INPUT_FILENAME)
+    print("Frequency: (Part 2)", freq)
